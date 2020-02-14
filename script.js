4d742934151026e86c9701412524b15e7cdf4518
LadyMarian <45603798+LadyMarian@users.noreply.github.com>
2020-02-14 14:25:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const newMessage = document.getElementById("delivered-message");
+
+function checkInputLength() {
+	if (input.value.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+function deliverMessageClick() {
+	if (checkInputLength()) {
+		newMessage.innerHTML = input.value;
+		input.value = "";
+	} else {
+		alert("Please write a message");
+	}
+}
+
+function deliverMessageKey(event) {
+	if (checkInputLength() && event.keyCode === 13) {
+		newMessage.innerHTML = input.value;
+		input.value = "";
+	} else if (checkInputLength() === false) {
+		alert("Please write a message");
+	}
+}
+
+button.addEventListener("click", deliverMessageClick);
+input.addEventListener("keypress", deliverMessageKey);
